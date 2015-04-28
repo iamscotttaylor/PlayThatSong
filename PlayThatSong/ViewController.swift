1ebcb4da14b3ef4dde4f3df7dcc2719fe3a20666
Scott Taylor <scott@staylor.st>
2015-04-28 22:26:23 +0100

    Updating multiple song capability

--- PlayThatSong/ViewController.swift
+++ PlayThatSong/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var currentSongLabel: UILabel!
     
     var audioSession: AVAudioSession!
-    var audioPlayer: AVAudioPlayer!
+//    var audioPlayer: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -59,26 +59,45 @@ class ViewController: UIViewController {
         }
     }
     
-    func configureAudioPlayer (){
-        
-        var songPath = NSBundle.mainBundle().pathForResource("Destiny (Fresh Direct's Chillstep Re", ofType: "mp3")
-        var songURL = NSURL.fileURLWithPath(songPath!)
-        
-        println("songURL: \(songURL)")
-        
-        var songError: NSError?
-        self.audioPlayer = AVAudioPlayer(contentsOfURL: songURL, error: &songError)
-        println("song error: \(songError)")
-        self.audioPlayer.numberOfLoops = 0
-        
-    }
+//    func configureAudioPlayer (){
+//        
+//        var songPath = NSBundle.mainBundle().pathForResource("Destiny (Fresh Direct's Chillstep Re", ofType: "mp3")
+//        var songURL = NSURL.fileURLWithPath(songPath!)
+//        
+//        println("songURL: \(songURL)")
+//        
+//        var songError: NSError?
+//        self.audioPlayer = AVAudioPlayer(contentsOfURL: songURL, error: &songError)
+//        println("song error: \(songError)")
+//        self.audioPlayer.numberOfLoops = 0
+//        
+//    }
     
     func playMusic () {
         
-        self.audioPlayer.prepareToPlay()
-        self.audioPlayer.play()
+//        self.audioPlayer.prepareToPlay()
+//        self.audioPlayer.play()
         
     }
 
+    func createSongs () -> [AnyObject] {
+        
+        let firstSongPath = NSBundle.mainBundle().pathForResource("Destiny (Fresh Direct's Chillstep Re", ofType: "mp3")
+        let secondSongPath = NSBundle.mainBundle().pathForResource("Nhumo_En mi Nube", ofType: "mp3")
+        let thirdSongPath = NSBundle.mainBundle().pathForResource("Nhumo_Weeping Willow", ofType: "mp3")
+        
+        let firstSongURL = NSURL.fileURLWithPath(firstSongPath!)
+        let secondSongURL = NSURL.fileURLWithPath(secondSongPath!)
+        let thirdSongURL = NSURL.fileURLWithPath(thirdSongPath!)
+        
+        let firstPlayItem = AVPlayerItem(URL: firstSongURL)
+        let secondPlayItem = AVPlayerItem(URL: secondSongURL)
+        let thirdPlayItem = AVPlayerItem(URL: thirdSongURL)
+        
+        let songs: [AnyObject] = [firstPlayItem, secondPlayItem, thirdPlayItem]
+        
+        return songs
+    }
+    
 }
 
