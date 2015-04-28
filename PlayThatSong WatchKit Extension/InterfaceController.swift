3cc2deff8fd0c4bd055e07145eebd13a7968afe5
Scott Taylor <scott@staylor.st>
2015-04-28 23:17:06 +0100

    adding some watchkit stuff

--- PlayThatSong WatchKit Extension/InterfaceController.swift
+++ PlayThatSong WatchKit Extension/InterfaceController.swift
@@ -11,7 +11,9 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
-
+    
+    @IBOutlet weak var songTitleLabel: WKInterfaceLabel!
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
@@ -26,6 +28,23 @@ class InterfaceController: WKInterfaceController {
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+    
+    //Mark - IBActions 
+    
+    
+    @IBAction func previousSongButtonPressed() {
+        
+    }
+    
+    @IBAction func nextSongButtonPressed() {
+        
+    }
+    
+    @IBAction func playSongButtonPressed() {
+        
+        
+        
     }
 
 }
