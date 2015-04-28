5a4f87dc880131be558a2dfe33d06e0e845f0d24
Scott Taylor <scott@staylor.st>
2015-04-28 22:12:06 +0100

    Initial Commit

--- PlayThatSong/ViewController.swift
+++ PlayThatSong/ViewController.swift
@@ -7,19 +7,78 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var currentSongLabel: UILabel!
+    
+    var audioSession: AVAudioSession!
+    var audioPlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.configureAudioSession()
+        self.configureAudioPlayer()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //Mark: - IBActions
+    
+    @IBAction func playButtonPressed(sender: UIButton) {
+        self.playMusic()
+        
+    }
+    @IBAction func playPreviousButtonPressed(sender: UIButton) {
+        
+    }
+    
+    @IBAction func playNextButtonPressed(sender: UIButton) {
+        
+    }
+    
+    //Mark: - Audio
+    
+    func configureAudioSession () {
+        self.audioSession = AVAudioSession.sharedInstance()
+        
+        var categoryError: NSError?
+        var activeError: NSError?
+        
+        self.audioSession.setCategory(AVAudioSessionCategoryPlayback, error: &categoryError)
+        println("error \(categoryError)")
+        
+        var success = self.audioSession.setActive(true, error: &activeError)
+        if !success {
+            println("Error making audio session active \(activeError)")
+        }
+    }
+    
+    func configureAudioPlayer (){
+        
+        var songPath = NSBundle.mainBundle().pathForResource("Destiny (Fresh Direct's Chillstep Re", ofType: "mp3")
+        var songURL = NSURL.fileURLWithPath(songPath!)
+        
+        println("songURL: \(songURL)")
+        
+        var songError: NSError?
+        self.audioPlayer = AVAudioPlayer(contentsOfURL: songURL, error: &songError)
+        println("song error: \(songError)")
+        self.audioPlayer.numberOfLoops = 0
+        
+    }
+    
+    func playMusic () {
+        
+        self.audioPlayer.prepareToPlay()
+        self.audioPlayer.play()
+        
+    }
 
 }
 
